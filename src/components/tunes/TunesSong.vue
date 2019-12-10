158d69124e34cbb17fa3381b347950e5c42f1f88
Martin Halaj <m.halaj11@gmail.com>
2019-12-10 14:15:16 +0100

    update 0.005

--- src/components/tunes/TunesSong.vue
+++ src/components/tunes/TunesSong.vue
@@ -1,15 +1,33 @@
 <template>
 	<article class="tunes-song">
-		<div v-if="song.cover">
-			<img :src="song.cover" alt="Album cover image" />
+		<div class="card mb-3" style="width: 520px;">
+			<div class="row no-gutters">
+				<div class="col-md-4" v-if="song.cover">
+					<img
+						:src="song.cover"
+						class="card-img"
+						alt="Album cover image"
+					/>
+				</div>
+				<div class="col-md-8">
+					<div class="card-body">
+						<h3 class="card-title">
+							{{ song.artist }}
+						</h3>
+						<figure v-if="song.audioFile">
+							<figcaption class=" mb-2">
+								{{ song.name }}
+							</figcaption>
+							<audio
+								class="shadow"
+								controls
+								:src="song.audioFile"
+							></audio>
+						</figure>
+					</div>
+				</div>
+			</div>
 		</div>
-		<p>
-			<strong>{{ song | songify }}</strong>
-		</p>
-		<figure v-if="song.audioFile">
-			<figcaption>Listen to the T-Rex:</figcaption>
-			<audio controls :src="song.audioFile"></audio>
-		</figure>
 	</article>
 </template>
 
@@ -27,9 +45,29 @@ export default {
 	}
 }
 </script>
-
 <style lang="scss" scoped>
-.tunes-song {
-	margin-bottom: 3em;
+.card-img {
+	width: 100%;
+	height: 100%;
+	overflow: hidden;
+}
+.card {
+	background-color: #252525;
+	display: block !important;
+	margin: 0 auto 2em !important;
+	background: {
+		image: url('../../assets/iTunes-logo.png');
+		repeat: no-repeat;
+		position-y: 2.6%;
+		position-x: 98.9%;
+		size: 1.4em;
+	}
+}
+audio {
+	border: none;
+	border-radius: 2em;
+}
+audio:focus {
+	outline: none;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,34 @@
 	<div class="home">
 		<img alt="iTunes logo" src="../assets/iTunes-logo.png" height="150" />
 		<hello-world msg="Welcome to Your Vue.js App" />
+		<tunes-song class="mt-5" :song="song" />
 	</div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/home/HelloWorld.vue'
+import TunesSong from '@/components/tunes/TunesSong.vue'
 
 export default {
 	name: 'home',
 	components: {
-		HelloWorld
+		HelloWorld,
+		TunesSong
+	},
+	data() {
+		return {
+			song: {
+				id: 339399242,
+				artist: 'Them Crooked Vultures',
+				audioFile:
+					'https://audio-ssl.itunes.apple.com/apple-assets-us-std-000001/Music/d2/42/38/mzm.aykywjep.aac.p.m4a',
+				cover:
+					'https://is1-ssl.mzstatic.com/image/thumb/Music/v4/8b/5b/ea/8b5bea91-f6b0-5255-9d38-3af2cec832ec/source/100x100bb.jpg',
+				name: 'No One Loves Me & Neither Do I',
+				album: 'Them Crooked Vultures (Bonus Track Version)'
+			}
+		}
 	}
 }
 </script>
